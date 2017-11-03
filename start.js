23061bb79c63b074040607fcc667197f22773df7
dallinmoak <dallinmoak@gmail.com>
2017-11-03 08:03:55 -0600

    the Javascript tutorials

--- start.js
+++ start.js
@@ -1,4 +1,46 @@
-var test = 'this is a test';
-console.log(test);
+//var thingObj = {
+//	val: "one",
+//};
+//
+//var thing = thingObj.val;
+//
+//function thingChanger(athing) {
+//	athing = "two";
+//	console.log("value in function: " + athing);
+//	return athing;
+//}
+//
+//console.log("original value: " + thingObj.val);
+//
+//var changed = thingChanger(thing);
+//
+//var maybeChanged = thingChanger(thingObj.val);
+//
+//console.log("using a variable: " + changed);
+//
+//console.log("using an object value: " + maybeChanged);
+//
+//console.log("original value: " + thingObj.val);
 
-console.log(2 + 5);
+var guide = {
+	title: 'Guide to Programming',
+	content: 'Content goes here...',
+	visibleToUser: function (viewingUserRole) {
+		if (viewingUserRole === 'paid') {
+			return true;
+		}
+	},
+	renderContent: function (userRole) {
+		if (this.visibleToUser(userRole)) {
+			console.log(this.title + " - " + this.content);
+		} else {
+			this.content = '';
+			console.log(this.title + " - " + this.content);
+		}
+	}
+}
+
+user = {
+	role: 'paid'
+};
+guide.renderContent(user.role);
